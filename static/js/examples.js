65fd390aaae259c465303989468870703951eb24
ejortizre7 <jairo.ortiz.9@gmail.com>
2018-10-10 03:59:11 +0200

    Funcionalidad Completa de la web
    La funcionalidad de SignUp, Login y Subir una imagen.

---
+++ static/js/examples.js
@@ -0,0 +1,127 @@
+'use strict'
+
+const basePath = 'http://localhost:3000'
+
+let token = ''
+
+function submitForm(formId) {
+  let formDom = document.querySelector('#' + formId)
+
+  let encType = formDom.getAttribute('x-enctype')
+  let target = formDom.getAttribute('x-target')
+  let method = formDom.getAttribute('x-method')
+
+  let data = {}
+
+  if (formId === 'signup'){
+    let form = new FormData(formDom)
+
+    let email = form.get('email')
+    let password = form.get('password')
+    let age = form.get('age')
+
+    data = {
+      email : email,
+      password : password,
+      age : age
+    }
+  }
+
+  if (formId === 'login'){
+    let form = new FormData(formDom)
+
+    let email = form.get('email')
+    let password = form.get('password')
+
+    data = {
+      email : email,
+      password : password
+    }
+  }
+
+  console.log('token', token)
+  console.log('encType', encType)
+  console.log('target', basePath + target)
+  console.log('data', data)
+
+  superagent
+  .post(basePath + target)
+  .set('x-access-token', token)
+  .set('Content-Type', encType)
+  .set('Accept', 'application/json')
+  .send(data)
+  .then(function(res) {
+    if (res.body.token) {
+      token = res.body.token
+    }
+    
+    if(formId=='login'){
+      document.cookie = setCookie("token",token,1);
+      window.location = "Perfil.html"
+    }
+    document.querySelector('#' + formId + '-log').value = JSON.stringify(res.body)
+    
+  })
+  .catch(function(e) {
+    console.error(e)
+    document.querySelector('#' + formId + '-log').value = JSON.stringify(e)
+  })
+}
+
+function upload() {
+  let file = document.querySelector('#image-uploader').files[0]
+  let token = getCookie("token")
+
+  superagent
+  .post('http://localhost:3000/image')
+  .set('x-access-token', token)
+  .field('token', token)
+  .attach('image', file, file.name)
+  .then(function(res) {
+    let source = getImage(res.body.url)
+
+    document.querySelector('#upload-demo').setAttribute('src', source)
+    document.querySelector('#upload-log').value = res.body.url
+    var i = document.createElement("img")
+    i.src= source;
+    document.querySelector('#columnaimagen').appendChild(i);
+
+  })
+}
+
+function getImage(url) {
+  return url + '?token=' + token
+}
+
+function download() {
+  let url = document.querySelector('#image-downloader').value
+  let source = getImage(url)
+
+  document.querySelector('#download-demo').setAttribute('src', source)
+  document.querySelector('#download-log').value = source
+
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+//
