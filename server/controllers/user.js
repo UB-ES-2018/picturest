5775e9f720d9c1a991260a1170156e7a3c378492
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2018-10-27 13:37:59 +0200

    Update due code deleted

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -25,28 +25,26 @@ exports.signup = function (req, res) {
 
     })
 
-    
-    if (exists) { 
-        res.status(409).send({error: "Someone already has this email address"}) 
-    }
-    if (!exists) {
-        user.save(function (err, saved) {
-            if (err) {
-                res.status(500).send({error: "Database error"})
-            }
-            if (saved) {
-                // if OK sends true
-                res.json({
-                    success: true,
-                    id: user._id,
-                })   
-            }
-        })
-    }
+    // saving into db
+    user.save(function (err, saved) {
+        if (err) {
+            res.json({
+                success: false,
+                error: "Someone already has this email address."
+            })
+        }
+        if (saved) {
+            // if OK sends true
+            res.json({
+                success: true,
+                id: user._id,
+            })
+        }
+    })
 }
 
 // login for the user
-exports.signin = function (req, res) {
+exports.login = function (req, res) {
     // find the user
     User.findOne({ email: req.body.email }).then(function (user, err) {
         if (err) {
@@ -167,5 +165,32 @@ exports.addProfileDesc = function(req, res) {
         }
     }).catch(function (err) {
         console.log(err)
+    })
+}
+
+exports.middleware = function (app) {
+    // route middleware to verify a token
+    app.use(function (req, res, next) {
+        // check header or url parameters or post parameters for token
+        var token = req.body.token || req.query.token || req.headers['x-access-token']
+        // decode token
+        if (token) {
+            // verifies secret and checks exp
+            jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.json({ error: true, message: 'Failed to authenticate token.' })
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded
+                    next()
+                }
+            })
+        } else {
+            // if there is no token return an error
+            return res.status(403).send({
+                error: true,
+                message: 'No token provided.'
+            })
+        }
     })
 }
